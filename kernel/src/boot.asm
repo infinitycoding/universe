a1894559866405a222774d56898d9e774815e7dc
Simon Diepold <info@infinitycoding.de>
2012-07-08 16:48:30 +0200

    1.0

--- kernel/src/boot.asm
+++ kernel/src/boot.asm
@@ -1,5 +1,5 @@
-;	Copyright 2012 the developers of the universe
-;	This file is part of Universe Kernel.
+;	Copyright 2012 universe coding group (UCG) all rights reserved
+;	This file is part of the Universe Kernel.
 ;
 ;    Universe Kernel is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
@@ -14,7 +14,9 @@
 ;    You should have received a copy of the GNU General Public License
 ;    along with Universe Kernel.  If not, see <http://www.gnu.org/licenses/>.
 ;
-;    Diese Datei ist Teil vom Universe Kernel.
+;	
+;	
+;    Diese Datei ist ein Teil vom Universe Kernel.
 ;
 ;    Das Universe Kernel ist Freie Software: Sie können es unter den Bedingungen
 ;    der GNU General Public License, wie von der Free Software Foundation,
@@ -29,6 +31,7 @@
 ;
 ;    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 ;    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+
 
 [BITS 32]
 section .text
