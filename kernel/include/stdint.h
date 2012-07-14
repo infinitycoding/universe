90db66d81c395957f7f3500accb01f46cda1939d
cedi <c_edi@gmx.de>
2012-07-15 00:44:58 +0300

    Update master

--- kernel/include/stdint.h
+++ kernel/include/stdint.h
@@ -21,49 +21,49 @@
 	
     Diese Datei ist ein Teil vom Universe Kernel.
 
-    Das Universe Kernel ist Freie Software: Sie können es unter den Bedingungen
+    Das Universe Kernel ist Freie Software: Sie kï¿½nnen es unter den Bedingungen
     der GNU General Public License, wie von der Free Software Foundation,
-    Version 3 der Lizenz oder jeder späteren
-    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+    Version 3 der Lizenz oder jeder spï¿½teren
+    verï¿½ffentlichten Version, weiterverbreiten und/oder modifizieren.
 
-    Das Universe Kernel wird in der Hoffnung, dass es nützlich sein wird, aber
-    Universe Kernel wird in der Hoffnung, dass es nützlich sein wird, aber
-    OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
-    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-    Siehe die GNU General Public License für weitere Details.
+    Das Universe Kernel wird in der Hoffnung, dass es nï¿½tzlich sein wird, aber
+    Universe Kernel wird in der Hoffnung, dass es nï¿½tzlich sein wird, aber
+    OHNE JEDE GEWï¿½HELEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewï¿½hrleistung der MARKTFï¿½HIGKEIT oder EIGNUNG Fï¿½R EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License fï¿½r weitere Details.
 
     Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
     Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
 */
 
-#define TRUE	1
-#define true	1
-#define FALSE	0
-#define false	0
-#define NULL 	0
+#define TRUE			1
+#define true			1
+#define FALSE			0
+#define false			0
+#define NULL 			0
 
-typedef unsigned char     uint8_t;
-typedef unsigned short    uint16_t;
-typedef unsigned int      uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned char     	uint8_t;
+typedef unsigned short    	uint16_t;
+typedef unsigned int      	uint32_t;
+typedef unsigned long long 	uint64_t;
 
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
+typedef signed char		int8_t;
+typedef signed short 		int16_t;
+typedef signed int 		int32_t;
+typedef signed long long 	int64_t;
 
-typedef unsigned int uintptr_t;
-typedef long int intptr_t;
+typedef unsigned int 		uintptr_t;
+typedef long int 		intptr_t;
 
-typedef int64_t intmax_t;
-typedef uint64_t uintmax_t;
+typedef int64_t 		intmax_t;
+typedef uint64_t 		uintmax_t;
 
-typedef unsigned int size_t;
+typedef unsigned int 		size_t;
 
-typedef unsigned int paddr_t;
+typedef unsigned int 		paddr_t;
 
-typedef int bool;
-typedef int boolean;
+typedef int 			bool;
+typedef int 			boolean;
 
 
 #endif
