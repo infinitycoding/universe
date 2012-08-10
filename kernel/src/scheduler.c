9e0de4ccf9177ba538c1b9d7835924443e385610
tdotu <simon.diepold@infinitycoding.de>
2012-08-10 14:54:01 +0200

    scheduling

--- kernel/src/scheduler.c
+++ kernel/src/scheduler.c
@@ -29,7 +29,7 @@ uint32_t *kernelstack;
 
 static struct task_state *proc0;
 static struct task_state *currentprocess;
-static struct zombepid *freepid=0;
+static struct zombiepid *freepid=0;
 static uint32_t pit_counter = 0;
 static bool lock= false;
 
@@ -56,12 +56,20 @@ void INIT_SCEDULER(void){
 	strcpy((char*)&proc0->name,"Kernel32.elf");
 	strcpy((char*)&proc0->description,"Kernel initialization");
     currentprocess = proc0;
-
+    pit_counter++;
 }
 
 struct cpu_state *task_schedul(struct cpu_state *cpu){
     /* TODO: Use Threads */
-    currentprocess->main_state = *cpu;
+
+
+    if(currentprocess->flags&TRASH){
+        //TODO: Free allocated memory and pagdir
+        free(currentprocess);
+    }else{
+        //save state
+        currentprocess->main_state = *cpu;
+    }
     currentprocess = currentprocess->next;
     while(!(currentprocess->flags & (ACTIV|FREEZED))){
         currentprocess=currentprocess->next;
@@ -86,13 +94,13 @@ struct cpu_state *task_schedul(struct cpu_state *cpu){
 
 pid_t proc_create(prev_t prev,vaddr_t vrt_base,paddr_t phy_base,size_t size,vaddr_t entrypoint,char* name,char* desc,priority_t priority) {
      struct task_state *proc_new = malloc(628);
-     proc_new->pagedir = pd_create();
-     memcpy(proc_new->pagedir, pd_kernel, 4096);
-
+    proc_new->pagedir = pd_create();
+    memcpy(proc_new->pagedir, pd_kernel, 4096);
 
      if(prev){ //kernelmode
-
-        pd_map_range(proc_new->pagedir,phy_base,vrt_base,PTE_WRITABLE,size/PAGE_SIZE);
+        if(size){
+            pd_map_range(proc_new->pagedir,phy_base,vrt_base,PTE_WRITABLE,size/PAGE_SIZE);
+        }
         proc_new->flags = KERNELMODE;
         struct cpu_state new_state = {
             .gs       = 0x10,
@@ -145,6 +153,7 @@ pid_t proc_create(prev_t prev,vaddr_t vrt_base,paddr_t phy_base,size_t size,vadd
 
      }
 
+
      strncpy((char*)&proc_new->name       , name, 255);
      strncpy((char*)&proc_new->description, desc, 255);
      proc_new->port          = NULL;
@@ -156,8 +165,23 @@ pid_t proc_create(prev_t prev,vaddr_t vrt_base,paddr_t phy_base,size_t size,vadd
      while(lock){}
      lock=true;
      asm volatile("cli");
-     //use zombe PIDs
-     proc_new->pid         = pit_counter;
+     //use zombie PIDs
+     if(freepid){
+        proc_new->pid = freepid->pid;
+        if(freepid->next == freepid){
+            free(freepid);
+            freepid=0;
+        }else{
+            freepid->prev->next = freepid->next;
+            freepid->next->prev = freepid->prev;
+            struct zombiepid *freepid_new = freepid->next;
+            free(freepid);
+            freepid = freepid_new;
+        }
+     }else{
+        proc_new->pid = pit_counter;
+     }
+
      proc_new->next        = proc0;
      proc_new->prev        = proc0->prev;
      proc0->prev           = proc_new;
@@ -170,29 +194,110 @@ pid_t proc_create(prev_t prev,vaddr_t vrt_base,paddr_t phy_base,size_t size,vadd
     return proc_new->pid;
 }
 
+static void proc_structure_free(struct task_state *proc){
+    if(proc->port){
+        struct port_access *current = proc->port->next;
+        struct port_access *next;
+        while(current != proc->port){
+            next = current->next;
+            free(current);
+            current = next;
+        }
+        free(proc->port);
+    }
+
+     if(proc->threads){
+        struct thread *current = proc->threads->next;
+        struct thread *next;
+        while(current != proc->threads){
+            next = current->next;
+            free(current);
+            current = next;
+        }
+        free(proc->threads);
+    }
+
+    if(proc->proc_children){
+        struct child *current = proc->proc_children->next;
+        struct child *next;
+        while(current != proc->proc_children){
+            next = current->next;
+            free(current);
+            current = next;
+        }
+        free(proc->proc_children);
+    }
+
+}
+
 
 void proc_kill(struct task_state *proc){
-    while(lock){}
-    lock=true;
+    if(proc){
+        while(lock){}
+        lock=true;
+        asm volatile("cli");
 
-    proc->prev->next = proc->next;
-    proc->next->prev = proc->prev;
+        proc->prev->next = proc->next;
+        proc->next->prev = proc->prev;
 
-    if(!freepid){
-        freepid = malloc(12);
-        freepid->prev = freepid;
-        freepid->next = freepid;
-        freepid->pid = proc->pid;
-    }else{
-        struct zombepid *zombepid_new = malloc(12);
-        zombepid_new->prev = freepid->prev;
-        zombepid_new->next = freepid;
-        freepid->prev->next = zombepid_new;
-        freepid->prev = zombepid_new;
+        if(!freepid){
+            freepid = malloc(12);
+            freepid->prev = freepid;
+            freepid->next = freepid;
+            freepid->pid = proc->pid;
+        }else{
+            struct zombiepid *zombiepid_new = malloc(12);
+            zombiepid_new->prev = freepid->prev;
+            zombiepid_new->next = freepid;
+            freepid->prev->next = zombiepid_new;
+            freepid->prev = zombiepid_new;
+        }
+        proc_structure_free(proc);
+        if(currentprocess != proc){
+            //TODO: Free allocated memory and pagdir
+            free(currentprocess);
+        }else{
+            proc->flags |= TRASH;
+        }
+        asm volatile("sti");
+        lock=false;
     }
-    //todo:
-    //free structure memory
-    //free PD memory
-
-    lock=false;
 }
+
+void exit(int errorcode){
+    proc_kill(currentprocess);
+    asm volatile("int $32");
+}
+
+
+struct task_state* proc_get(uint32_t pid){
+    if(proc0->pid == pid){
+        return proc0;
+    }
+
+	struct task_state* temp=proc0->next;
+	while(temp->next!=proc0->next && temp->pid != pid ){temp=temp->next;}
+	if(temp->pid != pid){
+		return NULL;
+    }
+	return temp;
+}
+
+uint32_t thread_create(uint32_t eip){
+
+}
+
+void thread_kill(struct thread *thr){
+
+
+}
+
+void thread_exit(int errorcode){
+
+}
+
+void thread_get(uint32_t tid){
+
+
+}
+
