2fb0742dc1766040ac61fe0f8d680258669682f6
Micha <micha.linuxfreak@googlemail.com>
2012-08-30 17:39:24 +0200

    extended vfs

--- kernel/include/sys/types.h
+++ kernel/include/sys/types.h
@@ -1,5 +1,5 @@
-#ifndef _vfs_h_
-#define _vfs_h_
+#ifndef _types_h_
+#define _types_h_
 /*
 	 Copyright 2012 universe coding group (UCG) all rights reserved
 	 This file is part of the Universe Kernel.
@@ -40,30 +40,14 @@
 	@author Michael Sippel <micha.linuxfreak@gmail.com>
 */
   #include <stdint.h>
-  #include <sys/stat.h>
-  #include <unistd.h>
   
-  typedef struct vfs_node {
-    char *name;
-    struct stat stat;
-    void *base;
-    uint32_t alloc;
-    struct vfs_node *parent;
-  } vfs_node_t;
-  
-  typedef struct vfs_dir_entry {
-    ino_t ino;
-    vfs_node_t *entry_node;
-  } vfs_dir_entry_t;
-  
-  void INIT_VFS(void);
-  void set_vfs_uid(uid_t new_uid);
-  void set_vfs_gid(gid_t new_gid);
-  vfs_node_t* vfs_create_node(char *name, mode_t mode, vfs_node_t *parent);
-  vfs_dir_entry_t* vfs_create_dir_entry(vfs_node_t *entry_node);
-  int vfs_write(vfs_node_t *node, void *base, int bytes);
-  void* vfs_read(vfs_node_t *node, uintptr_t offset);
-  int vfs_stat(vfs_node_t *node, struct stat *buffer);
-  int vfs_access(vfs_node_t *node, mode_t modus);
+  typedef unsigned int ino_t;
+  typedef unsigned int id_t;
+  typedef unsigned int uid_t;
+  typedef unsigned int gid_t;
+  typedef unsigned int mode_t;
+  typedef unsigned int nlink_t;
+  typedef signed int blkcnt_t;
+  typedef signed int off_t;
   
 #endif
--- kernel/include/unistd.h
+++ kernel/include/unistd.h
@@ -1,5 +1,5 @@
-#ifndef _vfs_h_
-#define _vfs_h_
+#ifndef _unistd_h_
+#define _unistd_h_
 /*
 	 Copyright 2012 universe coding group (UCG) all rights reserved
 	 This file is part of the Universe Kernel.
@@ -40,30 +40,10 @@
 	@author Michael Sippel <micha.linuxfreak@gmail.com>
 */
   #include <stdint.h>
-  #include <sys/stat.h>
-  #include <unistd.h>
   
-  typedef struct vfs_node {
-    char *name;
-    struct stat stat;
-    void *base;
-    uint32_t alloc;
-    struct vfs_node *parent;
-  } vfs_node_t;
-  
-  typedef struct vfs_dir_entry {
-    ino_t ino;
-    vfs_node_t *entry_node;
-  } vfs_dir_entry_t;
-  
-  void INIT_VFS(void);
-  void set_vfs_uid(uid_t new_uid);
-  void set_vfs_gid(gid_t new_gid);
-  vfs_node_t* vfs_create_node(char *name, mode_t mode, vfs_node_t *parent);
-  vfs_dir_entry_t* vfs_create_dir_entry(vfs_node_t *entry_node);
-  int vfs_write(vfs_node_t *node, void *base, int bytes);
-  void* vfs_read(vfs_node_t *node, uintptr_t offset);
-  int vfs_stat(vfs_node_t *node, struct stat *buffer);
-  int vfs_access(vfs_node_t *node, mode_t modus);
+  #define R_OK 0x1
+  #define W_OK 0x2
+  #define X_OK 0x4
+  #define F_OK 0x8
   
 #endif
