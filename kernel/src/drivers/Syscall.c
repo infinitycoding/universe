ec2e47d1b95409b4f406296e1b3140937ef71979
cedi <c_edi@gmx.de>
2012-12-25 01:49:45 +0100

    Create Syscall files. Code will come soon

---
+++ kernel/src/drivers/Syscall.c
@@ -0,0 +1,20 @@
+/*
+	 Copyright 2012 universe coding group (UCG) all rights reserved
+	 This file is part of the Universe Kernel.
+
+	 Universe Kernel is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 3 of the License, or
+	 any later version.
+
+	 Universe Kernel is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with Universe Kernel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <Syscall.h>
