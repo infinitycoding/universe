d27f2060c8c8743110b67d5141115ce752a0aa94
tdotu <simon.diepold@infinitycoding.de>
2013-09-15 16:24:56 +0200

    added syscall inline functions

--- user/include/universe.h
+++ user/include/universe.h
@@ -1,22 +1,50 @@
 #include <stdint.h>
 #include <stdarg.h>
 
-/*
-uint32_t linux_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi)
+
+#define SYS_EXIT 0
+#define SYS_FORK 1
+
+inline uint32_t linux_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi)
 {
+    uint32_t retv = 0;
     asm volatile(
-    "push %%ebx;"
-    "push %%esi;"
-    "push %%edi"
-    "int $128"
-    "pop %%edi"
-    "pop %%esi"
-    "pop %%ebx"
-    : "=a"
+    "push %ebx;"
+    "push %esi;"
+    "push %edi"
+    );
 
+    asm volatile(
+    "int $128;"
+    "pop %%edi;"
+    "pop %%esi;"
+    "pop %%ebx;"
+    : "=a" (retv) : "a" (function),  "b" (ebx), "c"(ecx), "d"(edx), "S"(esi), "D"(edi));
 
-}*/
+    return retv;
+}
 
 #define exit(RETV) asm volatile("int $128" : : "a"(0), "b"(RETV))
 #define thread_exit(RETV) asm volatile("int $112;": : "a"(0) , "b"(STR))
+
+
+inline uint32_t universe_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi)
+{
+    uint32_t retv = 0;
+    asm volatile(
+    "push %ebx;"
+    "push %esi;"
+    "push %edi"
+    );
+
+    asm volatile(
+    "int $112;"
+    "pop %%edi;"
+    "pop %%esi;"
+    "pop %%ebx;"
+    : "=a" (retv) : "a" (function),  "b" (ebx), "c"(ecx), "d"(edx), "S"(esi), "D"(edi));
+
+    return retv;
+}
+
 #define print(STR) asm volatile("pushl %%ebx; int $112; popl %%ebx;": : "a"(1) , "b"(STR))
