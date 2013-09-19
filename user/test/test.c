168e2457e8ee46ee775ff4f59733acae42dbdc56
Micha <micha.linuxfreak@googlemail.com>
2013-09-19 16:20:51 +0200

    syscalls: write, close

--- user/test/test.c
+++ user/test/test.c
@@ -2,11 +2,22 @@
 
 void _start(void) {
     print("Userspace!\nRead \"foo.txt\":\n");
-    int fd = linux_syscall(SYS_OPEN, "foo.txt", 1, 0x1ff, 0, 0);
-    char str[100] = "";
-    linux_syscall(SYS_READ, fd, &str, 13, 0, 0);
+
+    // open file
+    int fd = linux_syscall(SYS_OPEN, "foo.txt", 4, 0x1ff, 0, 0);
+    char str[12];
+
+    // read
+    linux_syscall(SYS_READ, fd, &str, 12, 0, 0);
     print(&str);
-    
+
+    // write
+    char str2[] = "New text!";
+    linux_syscall(SYS_WRITE, fd, &str, 10, 0, 0);
+
+    // close file
+    linux_syscall(SYS_CLOSE, fd, 0, 0, 0, 0);    
+
     exit(0);
 }
 
