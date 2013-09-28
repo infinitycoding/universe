763a333591b522ca68dbc338195d093f4be8adf4
tdotu <simon.diepold@infinitycoding.de>
2013-09-28 20:57:59 +0200

    scan PCI Base Adress Registers

--- kernel/include/drivers/pci.h
+++ kernel/include/drivers/pci.h
@@ -26,13 +26,53 @@
 #define PCI_CONFIG_DATA    0x0CFC
 #define PCI_CONFIG_ADDRESS 0x0CF8
 
-uint8_t pci_config_readb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset);
-uint16_t pci_config_readw(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset);
-uint32_t pci_config_readl(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset);
-void pci_config_writeb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint8_t value);
-void pci_config_writew(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint16_t value);
-void pci_config_writel(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint32_t value);
-bool pci_dev_exist(uint8_t bus, uint8_t dev);
+#define PCI_VENDOR_ID   0x00
+#define PCI_DEVICE_ID   0x02
+#define PCI_COMMAND     0x04
+#define PCI_STATUS      0x06
+#define PCI_REVISION    0x08
+#define PCI_CLASS       0x0B
+#define PCI_SUBCLASS    0x0A
+#define PCI_INTERFACE   0x09
+#define PCI_HEADERTYPE  0x0E
+#define PCI_BASE        0x10
+#define PCI_INTERRUPT   0x3C
+
+#define INTA 1
+#define INTB 2
+#define INTC 3
+#define INTD 4
+
+#define PCI_STANDART_HEADER  0
+#define PCI_PCI_BRIDGE_HEADER 1
+#define PCI_CARDBUS_BRIDGE_HEADER 2
+
+
+
+
+uint8_t pci_readb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset);
+uint16_t pci_readw(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset);
+uint32_t pci_readl(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset);
+void pci_writeb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint8_t value);
+void pci_writew(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint16_t value);
+void pci_writel(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint32_t value);
+pci_dev_exist(uint8_t bus, uint8_t dev, uint8_t func);
+
+typedef enum
+{
+    MEMORY = 0,
+    PORT = 1,
+}pci_base_t;
+
+
+struct pci_dev_base
+{
+    uint32_t adress;
+    pci_base_t type;
+    uint8_t resb;
+};
+
+
 
 void INIT_PCI();
 
@@ -40,6 +80,7 @@ struct pci_dev
 {
     uint8_t bus;
     uint8_t dev;
+    uint8_t func;
     uint16_t device_ID;
     uint16_t vendor_ID;
     uint8_t reversion_ID;
@@ -47,6 +88,10 @@ struct pci_dev
     uint8_t sub_class;
     uint8_t programming_interface;
     bool multifunc;
+    uint8_t header_type;
+    uint8_t irq_num;
+    uint8_t irq_pin;
+    struct pci_dev_base base_adress[6];
 };
 
 #endif
--- kernel/src/drivers/pci.c
+++ kernel/src/drivers/pci.c
@@ -38,7 +38,7 @@
  * @param Offset
  * @return value from the input adress
  */
-uint8_t pci_config_readb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
+uint8_t pci_readb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
 {
     outl(PCI_CONFIG_ADDRESS,0x80000000|(bus<<16)|(dev<<11)|(func<<8)|(offset&0xFC));
     return inb(PCI_CONFIG_DATA);
@@ -52,7 +52,7 @@ uint8_t pci_config_readb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
  * @param Offset
  * @return value from the input adress
  */
-uint16_t pci_config_readw(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
+uint16_t pci_readw(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
 {
     outl(PCI_CONFIG_ADDRESS,0x80000000|(bus<<16)|(dev<<11)|(func<<8)|(offset&0xFC));
     return inw(PCI_CONFIG_DATA);
@@ -67,7 +67,7 @@ uint16_t pci_config_readw(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
  * @param Offset
  * @return value from the input adress
  */
-uint32_t pci_config_readl(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
+uint32_t pci_readl(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
 {
     outl(PCI_CONFIG_ADDRESS,0x80000000|(bus<<16)|(dev<<11)|(func<<8)|(offset&0xFC));
     return inl(PCI_CONFIG_DATA);
@@ -82,7 +82,7 @@ uint32_t pci_config_readl(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset)
  * @param Offset
  * @param Value
  */
-void pci_config_writeb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint8_t value)
+void pci_writeb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint8_t value)
 {
     outl(PCI_CONFIG_ADDRESS,0x80000000|(bus<<16)|(dev<<11)|(func<<8)|(offset&0xFC));
     outb(PCI_CONFIG_DATA,value);
@@ -97,10 +97,10 @@ void pci_config_writeb(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint
  * @param Offset
  * @param Value
  */
-void pci_config_writew(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint16_t value)
+void pci_writew(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint16_t value)
 {
-    outl(PCI_CONFIG_ADDRESS,0x80000000|(bus<<16)|(dev<<11)|(func<<8)|(offset&0xFC));
-    outw(PCI_CONFIG_DATA,value);
+    outl(PCI_CONFIG_ADDRESS, 0x80000000 | (bus << 16) | (dev << 11) | (func << 8) | (offset & 0xFC));
+    outw(PCI_CONFIG_DATA, value);
 }
 
 
@@ -112,10 +112,10 @@ void pci_config_writew(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint
  * @param Offset
  * @param Value
  */
-void pci_config_writed(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint32_t value)
+void pci_writel(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint32_t value)
 {
-    outl(PCI_CONFIG_ADDRESS,0x80000000|(bus<<16)|(dev<<11)|(func<<8)|(offset&0xFC));
-    outl(PCI_CONFIG_DATA,value);
+    outl(PCI_CONFIG_ADDRESS, 0x80000000 | (bus << 16) | (dev << 11) | (func << 8) | (offset & 0xFC));
+    outl(PCI_CONFIG_DATA, value);
 }
 
 
@@ -125,9 +125,9 @@ void pci_config_writed(uint8_t bus,uint8_t dev,uint8_t func,uint8_t offset, uint
  *  @param Device/Slot
  *  @return true (device exists) or false (device does not exist)
  */
-bool pci_dev_exist(uint8_t bus, uint8_t dev)
+bool pci_dev_exist(uint8_t bus, uint8_t dev, uint8_t func)
 {
-    uint16_t vendor_ID = pci_config_readw(bus,dev,0,0);
+    uint16_t vendor_ID = pci_readw(bus, dev, func, 0);
     if(vendor_ID == 0 || vendor_ID == 0xFFFF)
         return false;
     return true;
@@ -146,35 +146,92 @@ list_t *pci_dev_list;
 
 void INIT_PCI()
 {
+    printf("PCI-devices:\n");
     pci_dev_list = list_create();
-    int dev,bus = 0;
+    int dev,bus,func;
 
     for(bus = 0; bus < 8; bus++)
     {
 
         for(dev = 0; dev < 32; dev++)
         {
-            int func;
 
-            if(pci_dev_exist(bus, dev))
+            for(func = 0; func < 8; func ++)
             {
-                uint32_t classcode = pci_config_readl(bus,dev,0,8);
 
-                struct pci_dev *current_dev = malloc(sizeof(struct pci_dev));
-                current_dev->bus = bus;
-                current_dev->dev = dev;
-                current_dev->device_ID = pci_config_readl(bus,dev,func,0)>>16;
-                current_dev->vendor_ID = pci_config_readw(bus,dev,func,0);
+                if(pci_dev_exist(bus, dev, func))
+                {
+                    bool multifunc = (pci_readb(bus, dev, func,PCI_HEADERTYPE) & 0x80) >> 7;
 
-                current_dev->reversion_ID = (uint8_t)classcode;
-                current_dev->programming_interface = (uint8_t) (classcode >> 8);
-                current_dev->sub_class = (uint8_t) (classcode >> 16);
-                current_dev->base_class = (uint8_t) (classcode >> 24);
+                    if(func && ! multifunc)
+                        continue;
 
-                list_push_front(pci_dev_list,current_dev);
+                    struct pci_dev *current_dev = malloc(sizeof(struct pci_dev));
 
+                    current_dev->bus = bus;
+                    current_dev->dev = dev;
+                    current_dev->func = func;
+
+                    uint32_t classcode = pci_readl(bus, dev, 0, 8);
+
+                    current_dev->reversion_ID = (uint8_t)classcode;
+                    current_dev->programming_interface = (uint8_t) (classcode >> 8);
+                    current_dev->sub_class = (uint8_t) (classcode >> 16);
+                    current_dev->base_class = (uint8_t) (classcode >> 24);
+
+                    current_dev->device_ID = pci_readw(bus, dev, func, PCI_DEVICE_ID);
+                    current_dev->vendor_ID = pci_readw(bus, dev, func, PCI_VENDOR_ID);
+                    current_dev->header_type = pci_readb(bus, dev ,0, PCI_HEADERTYPE) ^ 0x80;
+                    current_dev->multifunc = multifunc;
+
+
+                    // Standart Device
+                    if(! (current_dev->header_type & 0xFF) )
+                    {
+                        int base;
+
+                        for(base = 0; base < 6; base ++)
+                        {
+                            uint32_t current_base = pci_readl(bus, dev, func, PCI_BASE + (base * 4));
+
+                            // get type
+                            current_dev->base_adress[base].type = current_base & 1;
+
+                            // save current adress
+                            current_dev->base_adress[base].adress = current_base ^ 1;
+
+                            // get reserved bits
+                            pci_writel(bus, dev, func, PCI_BASE + (base * 4), 0xFFFFFFFF);
+                            uint32_t temp_base = pci_readl(bus, dev, func, PCI_BASE + (base * 4));
+                            temp_base = (~temp_base) | 1;
+                            current_dev->base_adress[base].resb = 0;
+
+                            int i;
+                            for(i = 0; i < 32; i++)
+                            {
+                                if(temp_base & (1 << i) == 0)
+                                    current_dev->base_adress[base].resb++;
+                                else
+                                    break;
+                            }
+
+                            //reset old state
+                             pci_writel(bus, dev, func, PCI_BASE + (base * 4), current_base);
+                        }
+
+
+                    }
+                    // Bridge Device
+                    else
+                    {
+                        //TODO: Write Cases for Different Headers
+                    }
+
+                    printf("device ID: %X  vendor ID: %X  bus: %d  port: %d  function: %d\n",current_dev->device_ID, current_dev->vendor_ID, current_dev->bus, current_dev->dev, current_dev->func);
+
+                    list_push_front(pci_dev_list,current_dev);
+                }
             }
-
         }
     }
 
