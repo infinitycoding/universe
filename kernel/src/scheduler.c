c9983cf81a2ae3b7ee57fafd71185215a34aabee
tdotu <simon.diepold@infinitycoding.de>
2013-09-30 12:51:16 +0200

    compiler fix

--- kernel/src/scheduler.c
+++ kernel/src/scheduler.c
@@ -62,7 +62,7 @@ void INIT_SCHEDULER(void)
     process_list = list_create();
     zombie_list = list_create();
     kernel_state = process_create("Kernel INIT", "initiate system", PROCESS_ACTIVE, NULL);
-    current_thread = thread_create(kernel_state, KERNELMODE, 0, NULL, 0, NULL);
+    current_thread = thread_create(kernel_state, KERNELMODE, 0, NULL, 0, 0,0);
 }
 
 /**
