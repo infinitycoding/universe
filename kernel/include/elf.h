cf3724e2164445776cba19c8264baf56390edf35
tdotu <info@infinitycoding.de>
2013-10-09 00:01:05 +0200

    warning fix for the process, elf, timer and irq moduel

--- kernel/include/elf.h
+++ kernel/include/elf.h
@@ -70,13 +70,13 @@ struct elf_header {
 	uint16_t machine;
 	uint32_t version;
 	uint32_t entry;
-	
+
 	uint32_t ph_offset;
 	uint32_t sh_offset;
-	
+
 	uint32_t flags;
 	uint16_t header_size;
-	
+
 	uint16_t ph_entry_size;
 	uint16_t ph_entry_count;
 	uint16_t sh_entry_size;
@@ -109,6 +109,6 @@ struct elf_program_header {
 	uint32_t alignment;
 } __attribute__((packed));
 
-struct process *load_elf(void *image);
+struct process_state *load_elf(void *image);
 
 #endif
