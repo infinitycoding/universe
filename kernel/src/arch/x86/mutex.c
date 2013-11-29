3f2ee0dcd83aa031396c511f7f8eb3ddc7083308
tdotu <info@infinitycoding.de>
2013-11-29 22:27:21 +0100

    added mutex functions for X86

---
+++ kernel/src/arch/x86/mutex.c
@@ -0,0 +1,54 @@
+/*
+    Copyright 2012 universe coding group (UCG) all rights reserved
+    This file is part of the Universe Kernel.
+    Universe Kernel is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+    Universe Kernel is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Universe Kernel.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/**
+
+  @author Simon Diepold aka. Tdotu (Universe Team) <simon.diepold@infinitycoding.de>
+
+ */
+
+#include <stdint.h>
+#include <arch/x86/arch_mutex.h>
+
+void lock(mutex m)
+{
+    asm volatile(
+        "mov $1, %%eax;"
+        "xchg %0 ,%%eax;"
+        :: "m" (*m)
+    );
+}
+
+void unlock(mutex m)
+{
+    asm volatile(
+        "mov $0, %%eax;"
+        "xchg %0 ,%%eax;"
+        :: "m" (*m)
+    );
+}
+
+
+bool try_lock(mutex m)
+{
+    bool r;
+    asm volatile(
+        "mov $1, %%eax;"
+        "xchg %0 ,%%eax;"
+        : "=a"(r) : "m" (*m)
+    );
+    return !r;
+}
