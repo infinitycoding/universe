789f308888672e5506522c04da93b2e3953fd4a5
tdotu <simon.diepold@infinitycoding.de>
2013-12-11 20:55:05 +0100

    fixed peters issue

--- libs/stdlibc/include/universe.h
+++ libs/stdlibc/include/universe.h
@@ -19,69 +19,15 @@
 #define SYS_UNLINK 10
 #define SYS_PIPE 11 // TODO
 
-#define exit(RETV) asm volatile("int $128" : : "a"(SYS_EXIT), "b"(RETV))
-
-inline uint32_t fork()
-{
-    uint32_t pid;
-    asm volatile("int $128;": "=a"(pid) : "a" (SYS_FORK));
-    return pid;
-}
-
-inline uint32_t alloc_memory(int pages)
-{
-	uint32_t addr;
-	asm volatile("int $112;" : "=a" (addr) : "a" (4), "b" (pages));
-	return addr;
-}
-
-inline uint32_t linux_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi)
-{
-    uint32_t retv = 0;
-    asm volatile(
-    "push %ebx;"
-    "push %esi;"
-    "push %edi"
-    );
-
-    asm volatile(
-    "int $128;"
-    "pop %%edi;"
-    "pop %%esi;"
-    "pop %%ebx;"
-    : "=a" (retv) : "a" (function),  "b" (ebx), "c"(ecx), "d"(edx), "S"(esi), "D"(edi));
-
-    return retv;
-}
-
-
-inline uint32_t universe_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi)
-{
-    uint32_t retv = 0;
-    asm volatile(
-    "push %ebx;"
-    "push %esi;"
-    "push %edi"
-    );
-
-    asm volatile(
-    "int $112;"
-    "pop %%edi;"
-    "pop %%esi;"
-    "pop %%ebx;"
-    : "=a" (retv) : "a" (function),  "b" (ebx), "c"(ecx), "d"(edx), "S"(esi), "D"(edi));
-
-    return retv;
-}
-
-void thread_exit(int retv) {
-  asm volatile("int $112;": : "a" (1) , "b"(retv));
-}
-
-#define print(STR) asm volatile("pushl %%ebx; int $112; popl %%ebx;": : "a"(0) , "b"(STR))
-#define thread_launch(FUNCTION, ARGC, ARGV) asm volatile("pushl %%ebx; int $112; popl %%ebx;": : "a"(2) , "b"(FUNCTION), "c"(ARGC), "d" (ARGV), "S" (&thread_exit))
-#define identify_os(...) universe_syscall(3,0,0,0,0,0)
 #define UNIVERSE_OS ('u' | ('n' << 8) | ('i' << 16))
-#define is ^0xFF000000 ==
 
+uint32_t linux_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi);
+uint32_t universe_syscall(uint32_t function, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi);
+uint32_t fork();
+uint32_t alloc_memory(int pages);
+void thread_exit(int retv);
+void exit(int retv);
+uint32_t identify_universe(void);
+void thread_launch(void * function, int argc, void *argv);
+void print(char *str);
 #endif
