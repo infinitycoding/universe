796a2721529d8a7b9cc204e005442b2a8477864b
BlitzBasic <phoesch@gmx.de>
2013-12-16 14:14:49 +0100

    make shell a targetrender

--- user/ultrashell/include/basicCMDs.h
+++ user/ultrashell/include/basicCMDs.h
@@ -5,6 +5,9 @@
 
 int echo(int argc, char **argv);
 int sver(int argc, char **argv);
+int add(int argc, char **argv);		// test only
+int sub(int argc, char **argv);		// test only
 int mul(int argc, char **argv);		// test only
+int div(int argc, char **argv);		// test only
 
 #endif
