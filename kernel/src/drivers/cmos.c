fe3edb886a7e9ff7428fef7ae091ed2605226068
Micha <michamimosa@gmail.com>
2013-12-31 11:09:29 +0100

    revise RTC-driver

--- kernel/src/drivers/cmos.c
+++ kernel/src/drivers/cmos.c
@@ -1,5 +1,5 @@
 /*
-	Copyright 2012 universe coding group (UCG) all rights reserved
+	Copyright 2012-2013 universe coding group (UCG) all rights reserved
 	This file is part of the Universe Kernel.
 
 	Universe Kernel is free software: you can redistribute it and/or modify
@@ -17,7 +17,7 @@
 */
 
 /**
-	@author Michael Sippel <micha.linuxfreak@gmail.com>
+	@author Michael Sippel <michamimosa@gmail.com>
 */
 
 #include <stdint.h>
@@ -25,52 +25,38 @@
 #include <drivers/timer.h>
 #include <drivers/cmos.h>
 
-cmos_data_t cmos;
-
 /**
  * Reads the Values from Complementary Metal Oxide Semiconductor
  *
- * @param void
+ * @param buffer
  * @return void
  */
-void INIT_CMOS(void) {
-	cmos.registers.register_a = cmos_read_byte(0x0A);
-	cmos.registers.register_b = cmos_read_byte(0x0B);
-	cmos.registers.register_c = cmos_read_byte(0x0C);
-	cmos.registers.register_d = cmos_read_byte(0x0D);
+void get_cmos_data(struct cmos_data *cmos) {
+	cmos->registers.register_a = cmos_read_byte(0x0A);
+	cmos->registers.register_b = cmos_read_byte(0x0B);
+	cmos->registers.register_c = cmos_read_byte(0x0C);
+	cmos->registers.register_d = cmos_read_byte(0x0D);
 
-	INIT_RTC();
-	cmos.time = get_time();
+	cmos->hardware.post_diagnostig_status_byte = cmos_read_byte(0x0E);
+	cmos->hardware.shutdown_status_byte =        cmos_read_byte(0x0F);
+	cmos->hardware.floppy_disk_type =            cmos_read_byte(0x10);
+	cmos->hardware.hd_type =                     cmos_read_byte(0x12);
+	cmos->hardware.device_byte =                 cmos_read_byte(0x14);
+	
+	cmos->hardware.basememory_size_low =         cmos_read_byte(0x15);
+	cmos->hardware.basememory_size_high =        cmos_read_byte(0x16);
+	cmos->hardware.expandablememory_size_low =   cmos_read_byte(0x17);
+	cmos->hardware.expandablememory_size_high =  cmos_read_byte(0x18);
+	cmos->hardware.extension_byte_hd1 =          cmos_read_byte(0x19);
+	cmos->hardware.extension_byte_hd2 =          cmos_read_byte(0x1A);
+	
+	cmos->hardware.cmos_magic_low =              cmos_read_byte(0x2E);
+	cmos->hardware.cmos_magic_high =             cmos_read_byte(0x2F);
+	
+	cmos->hardware.extendedmemory_low =          cmos_read_byte(0x30);
+	cmos->hardware.extendedmenory_high =         cmos_read_byte(0x31);
+}
 
-	cmos.hardware.post_diagnostig_status_byte = cmos_read_byte(0x0E);
-	cmos.hardware.shutdown_status_byte =        cmos_read_byte(0x0F);
-	cmos.hardware.floppy_disk_type =            cmos_read_byte(0x10);
-	cmos.hardware.hd_type =                     cmos_read_byte(0x12);
-	cmos.hardware.device_byte =                 cmos_read_byte(0x14);
-	
-	cmos.hardware.basememory_size_low =         cmos_read_byte(0x15);
-	cmos.hardware.basememory_size_high =        cmos_read_byte(0x16);
-	cmos.hardware.expandablememory_size_low =   cmos_read_byte(0x17);
-	cmos.hardware.expandablememory_size_high =  cmos_read_byte(0x18);
-	cmos.hardware.extension_byte_hd1 =          cmos_read_byte(0x19);
-	cmos.hardware.extension_byte_hd2 =          cmos_read_byte(0x1A);
-	
-	cmos.hardware.cmos_magic_low =              cmos_read_byte(0x2E);
-	cmos.hardware.cmos_magic_high =             cmos_read_byte(0x2F);
-	
-	cmos.hardware.extendedmemory_low =          cmos_read_byte(0x30);
-	cmos.hardware.extendedmenory_high =         cmos_read_byte(0x31);
-}
-/**
- * Get the CMOS-data
- * 
- * @param void
- *
- * @return pointer to the cmos-struct
- */
-cmos_data_t* get_cmos_data(void) {
-	return &cmos;
-}
 /**
  * Reads a byte from CMOS
  * @param offset Offset in the CMOS
@@ -82,6 +68,7 @@ uint8_t cmos_read_byte(uint8_t offset) {
 	outb(0x70, (tmp & 0x80) | (offset & 0x7F));
 	return inb(0x71);
 }
+
 /**
  * Write a byte to CMOS
  *
@@ -93,5 +80,6 @@ uint8_t cmos_read_byte(uint8_t offset) {
 void cmos_write_byte(uint8_t offset, uint8_t value) {
 	uint8_t tmp = inb(0x70);
 	outb(0x70, (tmp & 0x80) | (offset & 0x7F));
-	outb(0x71,value);
+	outb(0x71, value);
 }
+
