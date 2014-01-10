1faae0384b76f2c633fed6971ecce070d51ba7b6
tdotu <simon.diepold@infinitycoding.de>
2014-01-10 21:06:50 +0100

    added universal host

---
+++ kernel/include/drivers/hosts/uhost.h
@@ -0,0 +1,34 @@
+#ifndef _uhost_h_
+#define _uhost_h_
+
+/*
+	Copyright 2013 universe coding group (UCG) all rights reserved
+	This file is part of the Universe Kernel.
+
+	Universe Kernel is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	any later version.
+
+	Universe Kernel is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Universe Kernel.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <vfs.h>
+
+void INIT_UHOST(void);
+
+struct uhost_driver
+{
+    uint32_t ID_counter;
+    vfs_inode_t *stdin;
+    vfs_inode_t *stdout;
+};
+
+
+#endif
