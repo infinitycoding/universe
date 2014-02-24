f0eb12e67bc2277367b1e2079698b660ac4e971c
tdotu <simon.diepold@infinitycoding.de>
2014-02-24 15:18:17 +0100

    did some cleanup and debugging.

--- kernel/src/drivers/hosts/uhost.c
+++ kernel/src/drivers/hosts/uhost.c
@@ -13,9 +13,9 @@ void INIT_UHOST(int argc, void **argv)
 {
     printf("uhost-init sagt hallo! argc:%d \n", argc);
 
-    struct multiboot_struct *mb_info =  argv[1];
+    /*struct multiboot_struct *mb_info =  argv[1];
     char **drv_list =  argv[0];
-    printf("%s\n",drv_list[0]);
+    printf("%s\n",drv_list[0]);*/
 
     return; /// i don't know why, but it is necessary to return into kernel_thread_exit()
 }
