4f00e1139a420278725f01ce2f40df65202e0a0a
Peter Hösch <phoesch@gmx.de>
2014-04-26 10:16:35 +0200

    continue writing the pfp

--- kernel/include/pfp.h
+++ kernel/include/pfp.h
@@ -32,24 +32,23 @@
 
 
 
+#define NOTHING 0
 #define ESSENTIAL  1
 #define SERVICE  2
 #define KERNELROOT 4
+#define REPLACE 8
+#define APPEND 16
+#define UNDEFINED 32
+#define FAILTURE 64
 
 #define STRING_SECTION_LEN 7
+#define STRING_REPLACE_LEN 7
+#define STRING_APPEND_LEN 6
 
 
 
 typedef unsigned int ptype;
 
-
-
-struct ptype_ext
-{
-    ptype essential;
-    ptype service;
-    ptype kernelroot;
-};
 
 
 struct pnode
@@ -73,9 +72,14 @@ struct section
 list_t *pfp(char *pipeline_file);
 bool validate_pf(char *pipelines);
 int count_sections(char *pipelines);
-struct section parser_section(char *pipelines, char *section);
-struct ptype_ext extend_ptype(ptype type);
-ptype compress_ptype(struct ptype_ext type);
+struct section *parser_section(char *pipelines, int *section_pos);
+int find_next_section(char *pipelines, int *search_begin);
+bool is_whitespace(char character);
+int skip_whitespaces(char *pipelines, int *start);
+int count_sectionname_length(char *pipelines, int *start);
+ptype get_section_type(char *pipelines, int *start);
+bool check_section_type_given(char *pipelines, int *start);
+int skip_until_whitespace(char *pipelines, int *start);
 
 
 
