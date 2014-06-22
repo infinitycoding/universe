a53f58d59a1c05f683c80f01a9ba7353872347eb
tdotu <simon.diepold@infinitycoding.de>
2014-06-22 22:11:33 +0200

    added license disclaimers

--- libs/pfp/pfp.c
+++ libs/pfp/pfp.c
@@ -1,19 +1,19 @@
 /*
      Copyright 2012-2014 Infinitycoding all rights reserved
-     This file is part of the Universe Kernel.
+     This file is part of the PFP-library.
 
-     The Universe Kernel is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
+     The PFP-library is free software: you can redistribute it and/or modify
+     it under the terms of the GNU Lesser General Public License as published by
      the Free Software Foundation, either version 3 of the License, or
      any later version.
 
-     The Universe Kernel is distributed in the hope that it will be useful,
+     The PFP-library is distributed in the hope that it will be useful,
      but WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
+     GNU Lesser General Public License for more details.
 
-     You should have received a copy of the GNU General Public License
-     along with the Universe Kernel. If not, see <http://www.gnu.org/licenses/>.
+     You should have received a copy of the GNU Lesser General Public License
+     along with the PFP-library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
